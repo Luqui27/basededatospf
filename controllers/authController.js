9e5d0cdfea7988d7b1431f79388cf938df85903c
unknown <luqita.dmayo@gmail.com>
2023-11-30 09:56:51 -0300

    Agrego la respuesta isadmin al login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const secretKey = process.env.JWT_SECRET;
-const Usuario = require("../models/usuario");  // Asegúrate de importar el modelo de Usuario
+const Usuario = require("../models/usuario");
 
 const login = async (req, res) => {
   try {
@@ -11,12 +11,18 @@ const login = async (req, res) => {
     const usuario = await Usuario.findOne({ email });
 
     // Verificar si el usuario existe y la contraseña es válida
-    if (usuario && await bcrypt.compare(password, usuario.password)) {
+    if (usuario && (await bcrypt.compare(password, usuario.password))) {
       // Generar token de autenticación
-      const token = jwt.sign({ userId: usuario._id }, secretKey, { expiresIn: "1h" });
+      const token = jwt.sign({ userId: usuario._id }, secretKey, {
+        expiresIn: "1h",
+      });
 
-      // Enviar token como respuesta
-      res.json({ token });
+      // Incluir el isAdmin en la respuesta
+      res.json({
+        token,
+        isAdmin: usuario.isAdmin,
+        // Puedes incluir otros detalles del usuario aquí si es necesario
+      });
     } else {
       res.status(401).json({ error: "Credenciales inválidas" });
     }
