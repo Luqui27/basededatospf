85f1a9a443e36e7fa7bed82b57875fcb2ad3e328
unknown <luqita.dmayo@gmail.com>
2023-12-01 18:38:50 -0300

    devolver error 409 mail duplicado

--- controllers/registroController.js
+++ controllers/registroController.js
@@ -1,18 +1,23 @@
-const Usuario = require("../models/usuario");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const JWT_SECRET = process.env.JWT_SECRET;
-const saltRounds = 10;
-
 const registroController = async (req, res) => {
   try {
-    const { password, isAdmin, ...restoDatos } = req.body;
+    const { email, password, isAdmin, ...restoDatos } = req.body;
+
+    // Verificar si el correo electrónico ya existe
+    const usuarioExistente = await Usuario.findOne({ email });
+
+    if (usuarioExistente) {
+      // Si el correo electrónico ya está en uso, devuelve un estado 409 Conflict
+      return res.status(409).json({
+        error: "El correo electrónico ya está en uso. Por favor, utiliza otro.",
+      });
+    }
 
     // Hash de la contraseña
     const hashedPassword = await bcrypt.hash(password, saltRounds);
 
     // Crear un nuevo usuario
     const nuevoUsuario = new Usuario({
+      email,
       password: hashedPassword,
       isAdmin: isAdmin || false,
       ...restoDatos,
