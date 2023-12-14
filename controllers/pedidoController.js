4445a85ad93d3ecb97f97d6ac4b105fa8f6517f5
unknown <luqita.dmayo@gmail.com>
2023-12-13 21:55:44 -0300

    Rehaciendo logica para modificar pedidos

--- controllers/pedidoController.js
+++ controllers/pedidoController.js
@@ -46,6 +46,7 @@ const createPedido = async (req, res) => {
   }
 };
 
+// Actualizar un pedido existente
 const updatePedido = async (req, res) => {
   try {
     const { usuario, productos } = req.body;
@@ -57,23 +58,6 @@ const updatePedido = async (req, res) => {
     res.json({ pedido: pedidoActualizado });
   } catch (error) {
     res.status(500).json({ error: "Error al actualizar el pedido" });
-  }
-};
-
-// Nueva función para actualizar el estado 'Servido' de un pedido existente
-const updateServido = async (req, res) => {
-  try {
-    const { servido } = req.body;
-    const pedidoActualizado = await Pedido.findByIdAndUpdate(
-      req.params.id,
-      { servido },
-      { new: true }
-    );
-    res.json({ pedido: pedidoActualizado });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ error: "Error al actualizar el estado 'servido' del pedido" });
   }
 };
 
@@ -93,5 +77,4 @@ module.exports = {
   createPedido,
   updatePedido,
   deletePedido,
-  updateServido,
 };
