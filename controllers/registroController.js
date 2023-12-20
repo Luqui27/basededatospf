3ac27d61a8ed0c906e5c841fdfe42a0fae86bfe2
unknown <luqita.dmayo@gmail.com>
2023-12-20 13:05:31 -0300

    devolver error al mail duplicado

--- controllers/registroController.js
+++ controllers/registroController.js
@@ -1,12 +1,14 @@
-const Usuario = require("../models/usuario");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const JWT_SECRET = process.env.JWT_SECRET;
-const saltRounds = 10;
-
 const registroController = async (req, res) => {
   try {
     const { password, isAdmin, ...restoDatos } = req.body;
+
+    // Verificar si ya existe un usuario con el mismo correo electrónico
+    const existingUser = await Usuario.findOne({ email: restoDatos.email });
+    if (existingUser) {
+      return res.status(409).json({
+        error: 'El correo electrónico ya está en uso. Por favor, utiliza otro.',
+      });
+    }
 
     // Hash de la contraseña
     const hashedPassword = await bcrypt.hash(password, saltRounds);
@@ -25,31 +27,31 @@ const registroController = async (req, res) => {
     const token = jwt.sign(
       { id: nuevoUsuario._id, isAdmin: nuevoUsuario.isAdmin },
       JWT_SECRET,
-      { expiresIn: "1h" }
+      { expiresIn: '1h' }
     );
 
     // Setear el token en una cookie
-    res.cookie("token", token, { maxAge: 3600000, httpOnly: true });
+    res.cookie('token', token, { maxAge: 3600000, httpOnly: true });
 
     // Setear isAdmin en una cookie
-    res.cookie("isAdmin", nuevoUsuario.isAdmin, {
+    res.cookie('isAdmin', nuevoUsuario.isAdmin, {
       maxAge: 3600000,
       httpOnly: true,
     });
 
     //setear _id en una cookie
-    res.cookie("_id", nuevoUsuario._id, { maxAge: 3600000 });
+    res.cookie('_id', nuevoUsuario._id, { maxAge: 3600000 });
 
     // Enviar mensaje en la respuesta con el token y isAdmin
     res.status(200).json({
       token,
       isAdmin: nuevoUsuario.isAdmin,
       _id: nuevoUsuario._id,
-      mensaje: "Usuario registrado y logueado exitosamente",
+      mensaje: 'Usuario registrado y logueado exitosamente',
     });
   } catch (error) {
-    console.error("Error al registrar usuario:", error);
-    res.status(500).json({ error: "Error interno del servidor" });
+    console.error('Error al registrar usuario:', error);
+    res.status(500).json({ error: 'Error interno del servidor' });
   }
 };
 
